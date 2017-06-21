7d918eea7d9871dd05b6c7bc81e933b45490ca7a
Joshua Liu <liujoshua@users.noreply.github.com>
2017-06-21 15:59:19 -0700

    Demo sass/scss

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>Some stuff about this app</h1>
+  <div class="about">
+    <h1>Some stuff about this app</h1>
+  </div>
+
 </template>
+
+<style scoped lang="sass">
+  .about
+    h1
+      font-weight: 200
+</style>
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -15,22 +15,10 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style scoped lang="scss">
+  .hello {
+    h1 {
+      font-weight: 200;
+    }
+  }
 </style>
