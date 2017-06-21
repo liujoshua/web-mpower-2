0e6f4aa0ebf6f287e3968aae49188b2f2892a7b3
Joshua Liu <liujoshua@users.noreply.github.com>
2017-06-21 16:02:32 -0700

    add a 'footer'

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
       <li><router-link to="/About">About</router-link></li>
     </ul>
     <router-view></router-view>
+    <div class="footer">
+      <h2>This appears after router-view</h2>
+    </div>
   </div>
 </template>
 
