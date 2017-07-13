c4e2ed5ee4cc440b73c63b7c2987a83d78d00cee
Michael Lee <mtlee@Michaels-MacBook-Pro.local>
2017-07-13 16:12:30 -0700

    made template for single page app using bootstrap, responsive for desktop and mobile

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,53 @@
 
 <template>
+    <div id="app" class="container-fluid">
+    
+       <nav class="navbar fixed-top navbar-light bg-faded customNav">
+               <div class="navbar-header">
+                    <div class="row">
+                        <div class="col-md-4 hidden-sm-down">
+                            <h1> mPower </h1>
+                        </div>
+                        <div class="col-md-4 col-12 hidden-md-up" align="center">
+                            <h1> mPower </h1>
+                        </div>
 
-    <div id="app" class="container-fluid" >
+                        <div class="col-md-8 hidden-sm-down" align="right">
+                            <img src="./images/ic_check_circle_white_48px.svg" class="check hidden-sm-down"></img>
+                            <img src="./images/ic_assignment_black_36px.svg" class="hidden-sm-down board"></img>
+                            <img src="./images/ic_lock_black_48px.svg" class="hidden-sm-down"></img>
+                            <img src="./images/ic_lock_black_48px.svg" class="hidden-sm-down"></img>
+                            <img src="./images/ic_lock_black_48px.svg" class="hidden-sm-down"></img>  
+                        </div>
+                    </div>
+                    <div class="row hidden-md-up">
+                        <div class="col-12 bar" align="center">
+                            <img src="./images/ic_check_circle_white_48px.svg" class="check"></img>
+                            <img src="./images/ic_assignment_black_36px.svg" class="board"></img>
+                            <img src="./images/ic_lock_black_48px.svg"></img>
+                            <img src="./images/ic_lock_black_48px.svg"></img>
+                            <img src="./images/ic_lock_black_48px.svg"></img>
+                        </div>
+                    </div>
+            </div>
+        </nav>
+
+        <router-view class="router" ></router-view> <!--- TODO: must update to make sure application is only single page -->    
+
         <div class="row">
-            <div class="col-md-12">
-
-
-
-                <svg class="container" height = "75">
-                    <a xlink:href="#" >
-                    <text x=10 y=30 >mPower</text>
-                    </a>
-                    <circle cy="35" cx="1200" r="33" />
-                    <circle cy="35" cx="1100" r="33" />
-                    <circle cy="35" cx="1000" r="33"/>
-                    <circle cy="35" cx="900" r="33"/>
-                    <circle cy="35" cx= "800" r="33"/>
-                </svg>
-                <hr>
-                <router-view></router-view>
-
-                <div class="footer">
+            <div class="footer">
                 <h2> Copyright 2017 SageBionetworks</h2>
-                </div>
             </div>
         </div>
+
     </div>
-
-
 </template>
 
 
 <script>
-  export default {
-    name: 'app'
-  }
+    // export default {
+    //     name:  'app'
+    // }
 </script>
 
 <style src="./css/bootstrap.min.css"> </style>
--- src/components/SubjectInterest.vue
+++ src/components/SubjectInterest.vue
@@ -1,70 +1,91 @@
 <template>
 
-  <div class="page-overview">
-
-
-    <p> I'm interested in joining the mPower study because I </p>
+  <!--<div class="container-fluid">-->
+    <div class="page-overview text-left">
+      <p class="pageOne" > I'm interested in joining mPower because I </p>
     <br>
     <br>
-
-
-    <select v-model="selected">
+  
+    <select class="custom-select customizedSelect" v-model="selected">
       <option disabled value="">Please select one</option>
       <option> {{ hasParkinsons }}</option>
     </select>
-
-
-    <br><br>
-
-
-    <div v-if="seen">
-      <p> and I would be willing to try </p>
-
-      <br><br>
-      <select v-model="selected_option_two">
+  
+    <br>
+    <br>
+  
+    <div v-if="seen" >
+      <p class="pageOne"> and I would be willing to try </p>
+  
+      <br>
+      <br>
+      <select class="custom-select customizedSelect" id="pleaseSelect" v-model="selected_option_two">
         <option disabled value="">Please select one</option>
         <option>{{willing}}</option>
       </select>
-
+  
     </div>
-
-    <br><br>
+  
+    <br>
+    <br>
     <button id="next" v-on:click="clicked"> Next </button>
-
+  
   </div>
-
+  <!--</div>-->
 </template>
 
 <script>
-  export default {
-    name: 'subjectInterest',
-    data () {
-      return {
-        hasParkinsons: 'have parkinsons',
-        willing: 'this study to help further the research on Parkinsons',
-        selected: '',
-        selected_option_two: '',
-        seen: false
+// import Vue from 'vue'
+// var vm = new Vue({
+//   name: 'subjectInterest',
+//   data: {
+//     hasParkinsons: 'have parkinsons',
+//     willing: 'this study to help further the research on Parkinsons',
+//     selected: '',
+//     selected_option_two: '',
+//     seen: true
+//   },
+//   methods: {
+//     clicked: function () {
+//       // if (this.selected === this.hasParkinsons && !this.seen) {
+//       //   this.seen = true
+//       // }
+//       console.log('>>>>')
+//       console.log(this.selected === this.hasParkinsons)
+//       console.log(this.hasParkinsons)
+//       console.log('')
+//       if (this.selected_option_two === this.willing && this.seen) {
+//         window.location.href += `Eligibility`
+//       }
+//     }
+//   }
+// })
+// vm.seen = vm.seen
+export default {
+  name: 'subjectInterest',
+  data () {
+    return {
+      hasParkinsons: 'have parkinsons',
+      willing: 'this study to help further the research on Parkinsons',
+      selected: '',
+      selected_option_two: '',
+      seen: false
+    }
+  },
+  methods: {
+    clicked: function () {
+      if (this.selected === this.hasParkinsons && !this.seen) {
+        this.seen = true
       }
-    },
-    methods: {
-      clicked: function () {
-        if (this.selected === this.hasParkinsons && !this.seen) {
-          this.seen = true
-        }
-        if (this.selected_option_two === this.willing && this.seen) {
-          window.location.href += `Eligibility`
-        }
+      if (this.selected_option_two === this.willing && this.seen) {
+        window.location.href += `Eligibility`
       }
     }
   }
+}
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-  .hello {
-    h1 {
-      font-weight: 200;
-    }
-  }
+<style >
 </style>
